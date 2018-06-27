ccf2e859e91da73ef3c724adc1b5ae25d1194606
Jormthup <jomtup12@gmail.com>
2018-06-27 10:56:46 +0700

    jorm's first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { JormPage } from '../jorm/jorm';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoJormPage(){
+    this.navCtrl.push(JormPage);
   }
 
 }
