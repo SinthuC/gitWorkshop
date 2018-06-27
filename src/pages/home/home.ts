2d8bad1c0dbbc32d4019a9085630342bc20c70b2
sinthuC <sinthu_ch@kkumail.com>
2018-06-27 10:38:08 +0700

    ADD button and LINK to page pom

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PomPage } from '../pom/pom';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  goToPom(){
+    this.navCtrl.push(PomPage);
+  }
 }
